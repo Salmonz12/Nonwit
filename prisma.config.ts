a8c362b774caa81bb7fe801a3a4e1fd908a2976c
Salmonz12 <pear.jarunya@gmail.com>
2026-02-12 14:03:32 +0700

    init

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-
-  migrations: {
-    seed: "ts-node ./prisma/seed.ts",
+  datasource: {
+    url: process.env.DATABASE_URL!,
   },
 });
